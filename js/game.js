73cce7d9814338fb7ea5a27aece254c8dcaa9822
hai-an <1207783846@qq.com>
2022-08-17 22:29:27 +0800

    功能2.游戏对象创建

--- js/game.js
+++ js/game.js
@@ -1,5 +1,17 @@
 window.addEventListener( 'load', function ()
 {
-    console.log( '比懂得' )
-
+    let bigImage = document.querySelector( '#game' )
+    let birdDom = document.querySelector( '.brid' )
+    // 定义一个对象来表示背景的位置
+    const sky = {
+        position: 0
+    }
+    // 创建小鸟对象
+    const bird = {
+        entity: birdDom,
+        x: birdDom.offsetLeft,
+        y: birdDom.offsetTop,
+        speedX: 5,
+        speedY: 0
+    }
 } )

37cb4e3319aadafd2bba92b581a5917346374ecc
hai-an <1207783846@qq.com>
2022-08-17 23:06:01 +0800

    4.完成小鸟的移动功能开发

--- js/game.js
+++ js/game.js
@@ -6,6 +6,8 @@ window.addEventListener( 'load', function ()
     const sky = {
         position: 0
     }
+    // 表示重力加速度
+    const gravity = 1
     // 创建小鸟对象
     const bird = {
         entity: birdDom,
@@ -14,12 +16,22 @@ window.addEventListener( 'load', function ()
         speedX: 5,
         speedY: 0
     }
-    //
+
     var timer = setInterval( move, 20 )
     function move ()
     {
+        // 得到的值是负数 -5=0-5 背景向左移动
         sky.position = sky.position - bird.speedX
+        // 赋值操作
         bigImage.style.backgroundPositionX = sky.position + 'px'
+
+        //实现的小鸟在垂直方向上的移动
+        // 修改小鸟垂直方向上的速度,包含了重力加速度
+        bird.speedY = bird.speedY + gravity
+        // 修改了小鸟距离背景图片中的处理位置
+        bird.y = bird.y + bird.speedY
+        // 修改小鸟的垂直位置,实现了小鸟在垂直方向上的移动
+        bird.entity.style.top = bird.y + 'px'
     }
 
 } )

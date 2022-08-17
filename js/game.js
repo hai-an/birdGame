4b7c411c6537a121af1c50229289c4eddfb78953
hai-an <1207783846@qq.com>
2022-08-17 23:33:38 +0800

    5.完成上下边界的功能

--- js/game.js
+++ js/game.js
@@ -16,7 +16,7 @@ window.addEventListener( 'load', function ()
         speedX: 5,
         speedY: 0
     }
-
+    // 开启定时器
     var timer = setInterval( move, 20 )
     function move ()
     {
@@ -30,8 +30,26 @@ window.addEventListener( 'load', function ()
         bird.speedY = bird.speedY + gravity
         // 修改了小鸟距离背景图片中的处理位置
         bird.y = bird.y + bird.speedY
+        // 判断小鸟是否超出了下边界
+        // 判断方式:背景图的高度-小鸟的高度(下边界的范围),如果超出了这个范围表示小鸟超出了下边界
+        if ( bird.y > bigImage.offsetHeight - birdDom.offsetHeight )
+        {
+            bird.y = bigImage.offsetHeight - birdDom.offsetHeight
+            clearInterval( timer )
+        }
+        // 判断小鸟是否超出了上边界
+        //如果bird.y的值小于0,表示超出了上边界
+        //如果bird.y的值等于0,表示正好到达顶部
+        if ( bird.y < 0 )
+        {
+            bird.y = 0
+            clearInterval( timer )
+
+        }
         // 修改小鸟的垂直位置,实现了小鸟在垂直方向上的移动
         bird.entity.style.top = bird.y + 'px'
     }
+    // 给文档添加(连续)点击事件,使小鸟的不断变小
+    document.addEventListener( 'click', () => bird.speedY = -10 )
 
 } )
